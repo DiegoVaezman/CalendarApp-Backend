70fc6c0d8a81b7f3c2495206cd844c65cfc0f5ab
DiegoVaezman <diego@netspotofficial.com>
2023-01-07 11:29:07 +0100

    path * returns app added

--- index.ts
+++ index.ts
@@ -30,7 +30,7 @@ app.use('/api/auth',  authRouter);
 app.use('/api/events',  eventsRouter);
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 
